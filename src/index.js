e8fbd74deec8770dd775cf088dbc1bdc29d1e44e
Myfyn <mjastrzebski51@gmail.com>
2026-02-19 16:43:36 +0100

    lista filmów

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ const app = express()
 const port = 3005
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+});
+
+
+app.get("/api/movies", (req, res) => {
+
+    const movies = [
+        {
+            title: "Avatar",
+            director: "James Cameron"
+        },
+        {
+            title: "Interstellar",
+            director: "XYZ"
+        },
+    ];
+
+    res.json(movies);
 })
 
 app.listen(port, () => {
