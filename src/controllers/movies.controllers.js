ec2b2b9e5a6065c0f397b708d000a1fa238717f4
Myfyn <mjastrzebski51@gmail.com>
2026-02-20 13:17:14 +0100

    Like and dislke option

--- src/controllers/movies.controllers.js
+++ src/controllers/movies.controllers.js
@@ -19,7 +19,7 @@ export function createMovie(req, res, next) {
     title: movie.title,
     director: movie.director,
     likes: 0
-  })
+  });
   res.status(200).json({ message: "Dodano film", ...movie });
 }
 
@@ -30,5 +30,40 @@ export function getMovieById(req, res) {
     if (!movie) {
         return res.status(451).json({ message: "Unavailable for Legal Reasons" });
     }
+
+
     res.json(movie);
+}
+export function incrementLikes(req, res, next) {
+  const id = Number(req.params.id);
+
+  const movie = movies.find(m => m.id === id);
+
+  if (!movie) {
+    const err = new Error("Film nie znaleziony");
+    err.status = 404;
+    return next(err);
+  }
+
+  movie.likes++;
+
+  res.status(200).json(movie);
+}
+
+export function decrementLikes(req, res, next) {
+  const id = Number(req.params.id);
+
+  const movie = movies.find(m => m.id === id);
+
+  if (!movie) {
+    const err = new Error("Film nie znaleziony");
+    err.status = 404;
+    return next(err);
+  }
+
+  if (movie.likes > 0) {
+    movie.likes--;
+  }
+
+  res.status(200).json(movie);
 }
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { createMovie, getMovieById, getMovies } from "../controllers/movies.controllers.js";
 
+import {incrementLikes,decrementLikes,} from "../controllers/movies.controllers.js";
+
 const movieRoute = Router();
 
 movieRoute.get("/", getMovies);
 movieRoute.post("/", createMovie);
 movieRoute.get("/:id", getMovieById)
+movieRoute.patch("/:id/like", incrementLikes);
+movieRoute.patch("/:id/dislike", decrementLikes);
 
 export default movieRoute;
