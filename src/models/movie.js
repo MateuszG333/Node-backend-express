e3913436146298f221f6102553f064a167f3d02b
Myfyn <mjastrzebski51@gmail.com>
2026-02-21 11:12:34 +0100

    Update

--- src/models/movie.js
+++ src/models/movie.js
@@ -5,7 +5,7 @@ const movieSchema = new mongoose.Schema(
     {
         title: { type: String, required: true },
         description: { type: String, required: true },
-        year: { type: Number, required: false, default: 2026 },
+        year: { type: Number, required: false, default: 2026},
         director: { type: String, required: true },
         likes: { type: Number, default: 0 },
         },
